26de47f6fd8744ff317520a6253ea52ea324dfe3
Akihito Fujiwara <akitoha1207@gmail.com>
2018-06-05 17:10:03 +0900

    全員にpush通知送られるところまで

--- functions/index.js
+++ functions/index.js
@@ -7,6 +7,8 @@ const expo = new Expo();
 admin.initializeApp();
 
 const db = admin.database();
+const usersRef = db.ref('users');
+const messagesRef = db.ref('messages');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -16,22 +18,27 @@ const db = admin.database();
 // });
 
 exports.push = functions.https.onRequest((req, res) => {
-  db
-    .ref('users')
-    .once('value')
+  const { uid: senderUid, messageId } = req.query;
+  messagesRef.child(senderUid).child(messageId).once('value')
     .then(_ => _.val())
-    .then((users) => {
-      const messages = values(users).map(({ expoPushToken }) => {
-        return {
-          to: expoPushToken,
-          sound: 'default',
-          body: 'This is a test notification',
-          data: { withSome: 'data' },
-        };
-      });
-      const chunks = expo.chunkPushNotifications(messages);
-      chunks.map((chunk) => {
-        expo.sendPushNotificationsAsync(chunk);
-      });
+    .then(({ body }) => {
+      usersRef
+        .once('value')
+        .then(_ => _.val())
+        .then((users) => {
+          const messages = values(users).map(({ expoPushToken }) => {
+            return {
+              to: expoPushToken,
+              sound: 'default',
+              body,
+              data: { withSome: 'data' },
+            };
+          });
+          const chunks = expo.chunkPushNotifications(messages);
+          chunks.map((chunk) => {
+            expo.sendPushNotificationsAsync(chunk);
+          });
+          res.send('ok');
+        });
     });
 });
